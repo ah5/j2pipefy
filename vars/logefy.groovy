03f711a36deaa9cecc6db5229c767312f9bd9304
ah5 <aneesh.hamza@gmail.com>
2018-03-29 08:14:30 -0400

    updates to exception handling

--- vars/logefy.groovy
+++ vars/logefy.groovy
@@ -2,44 +2,52 @@
 
 /**
  * @description echoes info
- * @syntax logfy.info <message>
- * @usage logfy.info "info"
+ * @syntax logefy.info <message>
+ * @usage logefy.info "info"
  * 
  * @param message the message to be displayed
  */
 def info(message) {
+    if(stringefy.isEmpty(message)) error "logefy.info: param message cannot be empty"
     echo "INFO:" + valuefy.SPACE + message
 }
 
 /**
  * @description echoes warning
- * @syntax logfy.warning <message>
- * @usage logfy.warning "warning"
+ * @syntax logefy.warning <message>
+ * @usage logefy.warning "warning"
  * 
  * @param message the message to be displayed
  */
 def warning(message) {
+    if(stringefy.isEmpty(message)) error "logefy.warning: param message cannot be empty"
     echo "WARNING:" + valuefy.SPACE + message
 }
 
 /**
  * @description echoes debug
- * @syntax logfy.debug <message>
- * @usage logfy.debug "debug"
+ * @syntax logefy.debug <message>
+ * @usage logefy.debug "debug"
  * 
  * @param message the message to be displayed
  */
 def debug(message) {
+    if(stringefy.isEmpty(message)) error "logefy.debug: param message cannot be empty"
     echo "DEBUG:" + valuefy.SPACE + message
 }
 
 /**
  * @description echoes exception
- * @syntax logfy.exception <message>
- * @usage logfy.exception "exception"
+ * @syntax logefy.exception <message>
+ * @usage logefy.exception "exception"
  * 
- * @param message the message to be displayed
+ * @param ex the exception object
  */
-def exception(message) {
-    echo "EXCEPTION:" + valuefy.SPACE + ex.getMessage()
+def exception(ex) {
+    if(ex==null) error "logefy.exception: param ex cannot be null"
+    try {
+        echo "EXCEPTION:" + valuefy.SPACE + ex.getMessage()
+    } catch(exception) {
+        error "logefy.exception: exception getting message from exception object"
+    }
 }
--- vars/stringefy.groovy
+++ vars/stringefy.groovy
@@ -1,2 +1,25 @@
 #!/usr/bin/env groovy
 
+/**
+ * @description checks if string is empty
+ * @syntax stringefy.isEmpty <str>
+ * @usage stringefy.isEmpty "str"
+ * 
+ * @param str the string to be verified
+ * @return true if empty
+ */
+def isEmpty(str) {
+    return ((!str?.trim())? true: false)
+}
+
+/**
+ * @description checks if string is not empty
+ * @syntax stringefy.isNotEmpty <str>
+ * @usage stringefy.isNotEmpty "str"
+ * 
+ * @param str the string to be verified
+ * @return true if not empty
+ */
+def isNotEmpty(str) {
+    return ((str?.trim())? true: false)
+}
