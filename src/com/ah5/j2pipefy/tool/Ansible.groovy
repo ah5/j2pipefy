1d064c43a809c3469b9ba562ef23914739f7d829
ah5 <aneesh.hamza@gmail.com>
2018-04-09 23:39:39 -0400

    Added tool ansible.

--- src/com/ah5/j2pipefy/tool/Ansible.groovy
+++ src/com/ah5/j2pipefy/tool/Ansible.groovy
@@ -1,1 +1,29 @@
 #!/usr/bin/env groovy
+package com.ah5.j2pipefy.tool
+import groovy.transform.Field
+
+@Field def LOGGER = "com.ah5.j2pipefy.tool.Ansible"
+
+/**
+ * @description runs version command
+ */
+def version() {
+    logefy.info "${LOGGER}:version: Running version ..."
+    this.run("--version")
+}
+
+/**
+ * @description runs command string
+ *
+ * @param commandStr the command to run
+ */
+def run(commandStr) {
+    logefy.info "${LOGGER}:run: Running command ..."
+    if(stringefy.isEmpty(commandStr)) logefy.error "${LOGGER}:run: Param commandStr cannot be empty."
+    try {
+        sh "${valuefy.ANSIBLE_COMMAND} ${commandStr}"
+    } catch (ex) {
+        logefy.debug "${LOGGER}:run: Running ansible command failed !"
+        logefy.exception(ex)
+    }
+}
