1845957c4de76c96bdee71738a0c0b1c329d3f31
ah5 <aneesh.hamza@gmail.com>
2018-05-02 14:34:57 -0400

    updated testcases

--- test/com/ah5/j2pipefy/unit/TestJob.groovy
+++ test/com/ah5/j2pipefy/unit/TestJob.groovy
@@ -6,15 +6,25 @@ import com.lesfurets.jenkins.unit.BasePipelineTest
 
 class TestJob extends BasePipelineTest {
 
+    def script
+
     @Override
     @Before
     void setUp() throws Exception {
+        scriptRoots += 'test'
         super.setUp()
     }
 
     @Test
-    void testEcho() throws Exception {
-        def script = loadScript("workflows/echoJob.jenkins")
+    void testEchoNode() throws Exception {
+        script = loadScript("workflows/echoNode.jenkins")
+        script.execute()
+        printCallStack()
+    }
+
+    @Test
+    void testEchoPipeline() throws Exception {
+        script = loadScript("workflows/echoPipeline.jenkins")
         script.execute()
         printCallStack()
     }
